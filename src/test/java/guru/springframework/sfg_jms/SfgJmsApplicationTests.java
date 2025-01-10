2ef4c68c4815b6b223ed0e591ca02342d63ec423
tusharikagiri <126240171+tusharikagiri@users.noreply.github.com>
2025-01-10 15:43:56 +0530

    Added the project base code

---
+++ src/test/java/guru/springframework/sfg_jms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
